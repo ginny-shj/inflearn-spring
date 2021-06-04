60d494a26f1d9067dc5cb25149b6744efce58529
Ginny <d@ad.com>
2021-06-04 18:54:58 +0900

    spring introduction 06.04.Fri

--- src/main/java/me/shj/javaspring/SpringConfig.java
+++ src/main/java/me/shj/javaspring/SpringConfig.java
@@ -1,14 +1,22 @@
 package me.shj.javaspring;
 
 
+import me.shj.javaspring.repository.JdbcMemberRepository;
 import me.shj.javaspring.repository.MemberRepository;
 import me.shj.javaspring.repository.MemoryMemberRepository;
 import me.shj.javaspring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberSerivce(){
@@ -17,7 +25,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
---
+++ src/test/java/me/shj/javaspring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,64 @@
+package me.shj.javaspring.service;
+
+import me.shj.javaspring.domain.Member;
+import me.shj.javaspring.repository.MemberRepository;
+import me.shj.javaspring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest //Spring컨터이너를 같이 띄워서 테스트
+@Transactional  //db에 커밋하지 않고 롤백
+public class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("ginny");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertEquals(findMember.getName(), member.getName());
+
+    }
+
+    @Test
+    void 중복확인(){
+        //given
+        Member member1 = new Member();
+        member1.setName("ginny");
+
+        Member member2 = new Member();
+        member2.setName("ginny");
+
+        //when
+        memberService.join(member1);
+        Assertions.assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+//        try{
+//            memberService.join(member2);
+//        } catch(IllegalStateException e) {
+//            Assertions.assertEquals("이미 존재하는 회원입니다.", e.getMessage())
+//        }
+
+
+    }
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
